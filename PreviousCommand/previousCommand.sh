7900d3023528e61f071a0d5950b9d6ed478ebcfd
Sujay R <sujay-pt7892@sujay-pt7892.csez.zohocorpin.com>
2025-02-18 13:34:03 +0530

    switch case

--- PreviousCommand/previousCommand.sh
+++ PreviousCommand/previousCommand.sh
@@ -6,3 +6,10 @@ if [ $? -eq 0 ]; then
 else
     echo "File does not exist"
 fi
+
+
+cd testdir
+case $? in
+    0) echo "Directory created successfully." ;;
+    *) echo "Failed to create directory." ;;
+esac
