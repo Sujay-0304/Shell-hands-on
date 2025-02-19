18e74fe637034dcf54fce4731bffe2e05d596549
Sujay R <sujay-pt7892@sujay-pt7892.csez.zohocorpin.com>
2025-02-19 18:57:59 +0530

    explored all commands grep

---
+++ Allcommands/allCommands.sh
@@ -0,0 +1,21 @@
+
+# echo $(ls -la)
+# echo $(cat sample.txt)
+# echo $(pwd)
+# # echo $(vi)
+# echo $(less sample.txt)
+# echo $(more sample.txt)
+
+# echo $(grep words sample.txt)
+# echo $(grep -rin  error /var/log)   # -r is recursive,(alldirectories)  -i is case insensitive, -n is line number
+# echo $(grep -rinw  error /var/log | wc -w)   # -r is recursive,(alldirectories)  -i is case insensitive, -n is line number -w is whole word
+# echo $(grep -oi wordS sample.txt)
+# echo $(grep -oi words sample.txt | wc -w)
+# printf "%s\n" $(grep -ro --include="*.log" "error" /var/log/)
+# echo $(awk '{print $1 $2}' sample.txt)
+# echo $(sed 's/sujay/sujay1/' sample.txt)
+# echo $(cut -d " " -f 1 sample.txt)
+echo $(find ~/tutorial-bash/ -name "sample.txt") # find file in directory and outputs the path
+
+# printf "%s\n" $(grep -E "[a-zA-Z0-9]+.[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]"  emails.txt) 
+grep -Eo "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" emails.txt
